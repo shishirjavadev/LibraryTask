e347277c8782a92e66572be3c3977347fedafadf
shishirjavadev <65672937+shishirjavadev@users.noreply.github.com>
2020-05-21 23:59:58 +0530

    Update AbstractTest.java

--- library/src/test/java/com/book/library/AbstractTest.java
+++ library/src/test/java/com/book/library/AbstractTest.java
@@ -1,10 +1,13 @@
 package com.book.library;
 
-import java.io.IOException;
-
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.library.book.LibraryApplication;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -12,15 +15,14 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = LibraryApplication.class)
 @WebAppConfiguration
+@AutoConfigureMockMvc
 public abstract class AbstractTest {
+    @Autowired
     protected MockMvc mvc;
     @Autowired
     WebApplicationContext webApplicationContext;
